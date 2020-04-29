4737ac4e44b35edc0ccb107efcf602dd887d3125
varlanan <andreea.varlan98@gmail.com>
2020-04-29 00:31:06 -0400

    Login page GUI fixed with constraintlayout instead of relativelayout

--- app/src/main/java/com/fist/cineyet/MainActivity.java
+++ app/src/main/java/com/fist/cineyet/MainActivity.java
@@ -3,19 +3,24 @@ package com.fist.cineyet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText Email;
+    private EditText Password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView facebook = findViewById(R.id.FacebookLogo);
-        facebook.bringToFront();
-
-        ImageView google = findViewById(R.id.GoogleLogo);
-        google.bringToFront();
+//        EditText emailLogin = (EditText) findViewById(R.id.EmailLogin);
+//        ImageView facebook = findViewById(R.id.FacebookLogo);
+//        facebook.bringToFront();
+//
+//        ImageView google = findViewById(R.id.GoogleLogo);
+//        google.bringToFront();
     }
 }
