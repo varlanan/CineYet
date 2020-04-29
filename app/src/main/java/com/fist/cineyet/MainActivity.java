2fabb6ca62ce7a896b608d74338e4d6818a2bfb2
yifei-tang <yifeit.tang@mail.utoronto.ca>
2020-04-28 22:41:10 -0600

    idek what's going on

--- app/src/main/java/com/fist/cineyet/MainActivity.java
+++ app/src/main/java/com/fist/cineyet/MainActivity.java
@@ -3,24 +3,86 @@ package com.fist.cineyet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.EditText;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Button; //package for button
+import android.view.View;
+import android.content.Intent;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText Email;
-    private EditText Password;
-
+    private Button loginButton;
+    private static final String TAG="TAG";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
-//        EditText emailLogin = (EditText) findViewById(R.id.EmailLogin);
-//        ImageView facebook = findViewById(R.id.FacebookLogo);
-//        facebook.bringToFront();
-//
-//        ImageView google = findViewById(R.id.GoogleLogo);
-//        google.bringToFront();
+        ImageView facebook = findViewById(R.id.FacebookLogo);
+        facebook.bringToFront();
+
+        ImageView google = findViewById(R.id.GoogleLogo);
+        google.bringToFront();
+        loginButton=(Button) findViewById(R.id.LoginButton);
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openProfilePage();
+            }
+        });
+
+        Log.i(TAG,"onCreate");
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG,"onStart");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG,"onResume");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause");
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop");
+    }
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart");
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
+    }
+
+    public void openProfilePage(){
+        Intent myintent=new Intent(this, profile_page.class);
+        Log.i(TAG,"openProfilePage");
+
+        startActivity(myintent);
+
     }
 }
