4ce5c42005d6873204d08a3997074c10e98225b7
yifei-tang <yifeit.tang@mail.utoronto.ca>
2020-05-01 18:13:35 -0600

    home page layout done

--- app/src/main/java/com/fist/cineyet/HomeFragment.java
+++ app/src/main/java/com/fist/cineyet/HomeFragment.java
@@ -8,11 +8,40 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
+
+    RecyclerView favouriteMoviesLayout;
+    MainAdapter mainAdapter;
+    View myview;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        myview= inflater.inflate(R.layout.fragment_home, container, false);
+        Integer[] moviesArray={R.drawable.groundhogdayposter,R.drawable.movieposter,R.drawable.rearwindowposter,R.drawable.serbianfilmposter,R.drawable.parasiteposter};
+        Integer[] movies2Array={R.drawable.boanposter,R.drawable.littlewomen,R.drawable.midsommarposter,R.drawable.oldboyposter};
+
+        scrollFunction(R.id.friends_rec,moviesArray);
+        scrollFunction(R.id.mood_rec,movies2Array);
+        return myview;
+    }
+
+    private void scrollFunction(Integer id, Integer[] moviesArray){
+        favouriteMoviesLayout=(RecyclerView)  myview.findViewById(id);
+        ArrayList<Integer> moviesList=new ArrayList<Integer>();
+        for(int i=0;i<moviesArray.length;i++){
+            moviesList.add(moviesArray[i]);
+        }
+        moviesList.add(R.drawable.plusbutton);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(),LinearLayoutManager.HORIZONTAL,false);
+        favouriteMoviesLayout.setLayoutManager(layoutManager);
+        favouriteMoviesLayout.setItemAnimator(new DefaultItemAnimator());
+        mainAdapter= new MainAdapter(getActivity(),moviesList);
+        favouriteMoviesLayout.setAdapter(mainAdapter);
     }
 }
