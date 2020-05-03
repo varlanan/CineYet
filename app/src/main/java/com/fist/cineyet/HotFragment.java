228a231b941de546b654c3a419c6afa340ea09aa
yifei-tang <yifeit.tang@mail.utoronto.ca>
2020-05-02 22:15:57 -0600

    finished newsfeed setup

--- app/src/main/java/com/fist/cineyet/HotFragment.java
+++ app/src/main/java/com/fist/cineyet/HotFragment.java
@@ -8,12 +8,37 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class HotFragment extends Fragment {
+
+    View myview;
+    RecyclerView newsfeedLayout;
+    newsFeedAdapter mainAdapter;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_hot, container, false);
+        myview= inflater.inflate(R.layout.fragment_hot, container, false);
+        ArrayList<newsfeedItems> myMovies=new ArrayList<newsfeedItems>();
+        for(int i=0;i<10;i++){
+            myMovies.add(new newsfeedItems("Sept-06-2018", "YiFei Tang", "Midsommar",  "Midsommar scared the shit out of me. What the hell was that",
+                    "Reviewed",R.drawable.midsommarposter,R.drawable.roundprofilepic));
+        }
+        scrollFunction(R.id.newsFeed,myMovies);
+        return myview;
+    }
+
+    private void scrollFunction(Integer id, ArrayList<newsfeedItems> moviesList){
+        newsfeedLayout=(RecyclerView) myview.findViewById(id);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(),LinearLayoutManager.VERTICAL,false);
+        newsfeedLayout.setLayoutManager(layoutManager);
+        newsfeedLayout.setItemAnimator(new DefaultItemAnimator());
+        mainAdapter= new newsFeedAdapter(getActivity(),moviesList);
+        newsfeedLayout.setAdapter(mainAdapter);
     }
 }
 
