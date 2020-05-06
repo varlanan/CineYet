41af60fb5054dbc48eac3beb9712a305881dc58b
yifei-tang <yifeit.tang@mail.utoronto.ca>
2020-05-05 18:15:46 -0600

    profile picture can be updated, name reflects whats on firebase

--- app/src/main/java/com/fist/cineyet/UpdateProfileActivity.java
+++ app/src/main/java/com/fist/cineyet/UpdateProfileActivity.java
@@ -3,6 +3,7 @@ package com.fist.cineyet;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,13 +16,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.signin.internal.Storage;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class UpdateProfileActivity extends AppCompatActivity {
@@ -29,12 +37,13 @@ public class UpdateProfileActivity extends AppCompatActivity {
     EditText username;
     EditText interests;
     EditText email;
-    ImageView profile_pic;
+    CircleImageView profile_pic;
     Button submit;
     Button update_profile_pic;
     FirebaseAuth mAuth;
     String currentUserID;
     final static int gallery_pick=1;
+    final static String TAG="TAG";
     private DatabaseReference UserRef;
     private StorageReference UserProfileImageRef;
 
@@ -84,6 +93,23 @@ public class UpdateProfileActivity extends AppCompatActivity {
             }
         });
 
+        UserRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Log.i(TAG,"on data change");
+
+                if(dataSnapshot.exists()){
+                    String image_string=dataSnapshot.child("profileimage").getValue().toString();
+                    Picasso.get().load(image_string).placeholder(R.drawable.ic_account_circle_black_24dp).into(profile_pic);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
     }
 
@@ -92,28 +118,48 @@ public class UpdateProfileActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode==gallery_pick && resultCode==RESULT_OK && data!=null){
             Uri ImageUri=data.getData();
+
             final StorageReference filePath=UserProfileImageRef.child(currentUserID+".jpg");
-            filePath.putFile(ImageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+            filePath.putFile(ImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+
                 @Override
-                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
-                     if(task.isSuccessful()) {
-                         Toast.makeText(UpdateProfileActivity.this, "Profile Image Successfully Uploaded to storage in Firebase", Toast.LENGTH_SHORT).show();
-                         final String downloadUrl=filePath.getDownloadUrl().toString();
-                         UserRef.child("profileimage").setValue(downloadUrl).addOnCompleteListener(new OnCompleteListener<Void>() {
-                             @Override
-                             public void onComplete(@NonNull Task<Void> task) {
-                                 if(task.isSuccessful())
-                                     Toast.makeText(UpdateProfileActivity.this, "Profile Image Successfully Stored in database", Toast.LENGTH_SHORT).show();
-                                 else{
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    filePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
 
-                                     String message =task.getException().getMessage();
-                                     Toast.makeText(UpdateProfileActivity.this, "Error Occurred"+message, Toast.LENGTH_SHORT).show();
+                        @Override
 
-                                 }
+                        public void onSuccess(Uri uri) {
 
-                             }
-                         });
-                     }
+                            final String downloadUrl = uri.toString();
+
+                            UserRef.child("profileimage").setValue(downloadUrl).addOnCompleteListener(new OnCompleteListener<Void>() {
+
+                                @Override
+
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+
+
+                                        Toast.makeText(UpdateProfileActivity.this, "Image Stored", Toast.LENGTH_SHORT).show();
+
+
+                                    } else {
+
+                                        String message = task.getException().getMessage();
+
+                                        Toast.makeText(UpdateProfileActivity.this, "Error:" + message, Toast.LENGTH_SHORT).show();
+
+
+                                    }
+
+                                }
+
+                            });
+
+                        }
+
+
+                    });
                 }
             });
         }
--- app/src/main/java/com/fist/cineyet/profile_page.java
+++ app/src/main/java/com/fist/cineyet/profile_page.java
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,8 +22,16 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class profile_page extends Fragment {
@@ -30,11 +39,16 @@ public class profile_page extends Fragment {
     RecyclerView favouriteMoviesLayout;
     ListView activityList;
     MainAdapter mainAdapter;
-
+    TextView name;
+    TextView interests;
     Button bLogOut;
     Button editProfile;
-
+    CircleImageView profile_pic;
+    DatabaseReference UserRef;
+    FirebaseAuth mAuth;
+    String currentUserID;
     FirebaseAuth myFirebaseAuth;
+    String new_name;
     private FirebaseAuth.AuthStateListener myAuthListener;
 
 
@@ -56,11 +70,19 @@ public class profile_page extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        //hardcoded for now
         myview= inflater.inflate(R.layout.activity_profile_page, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+        UserRef=FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserID);
+        name = myview.findViewById(R.id.profile_pic_name);
+        interests = myview.findViewById(R.id.profile_interests);
         bLogOut = (Button) myview.findViewById(R.id.LogOutButton);
         editProfile=(Button)myview.findViewById(R.id.update_profile);
+        profile_pic=myview.findViewById(R.id.profile_picture_sample);
+        new_name=name.getText().toString();
         bLogOut.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 FirebaseAuth.getInstance().signOut();
@@ -71,18 +93,43 @@ public class profile_page extends Fragment {
         editProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent meinTent = new Intent((Context)getActivity(), UpdateProfileActivity.class);
-                startActivity(meinTent);
+                Intent myIntent = new Intent((Context)getActivity(), UpdateProfileActivity.class);
+                startActivity(myIntent);
             }
         });
 
+        UserRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if(dataSnapshot.exists()){
+                    String image_string=dataSnapshot.child("profileimage").getValue().toString();
+                    Picasso.get().load(image_string).placeholder(R.drawable.ic_account_circle_black_24dp).into(profile_pic);
+
+
+                    new_name=dataSnapshot.child("name").getValue().toString();
+                    name.setText(new_name);
+
+                    if(dataSnapshot.child("interests").getValue()!=null) {
+                        String new_interests = dataSnapshot.child("interests").getValue().toString();
+                        interests.setText(new_interests);
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
 
         Integer[] moviesArray={R.drawable.groundhogdayposter,R.drawable.movieposter,R.drawable.rearwindowposter,R.drawable.serbianfilmposter,R.drawable.parasiteposter};
         Integer[] movies2Array={R.drawable.boanposter,R.drawable.littlewomen,R.drawable.midsommarposter,R.drawable.oldboyposter};
         ArrayList<newsfeedItems> myMovies=new ArrayList<newsfeedItems>();
         for(int i=0;i<10;i++){
-            myMovies.add(new newsfeedItems("Sept-06-2018", "YiFei Tang", "Midsommar",  "Midsommar scared the shit out of me. What the hell was that",
+            myMovies.add(new newsfeedItems("Sept-06-2018", new_name, "Midsommar",  "Midsommar scared the shit out of me. What the hell was that",
                     "Reviewed",R.drawable.midsommarposter,R.drawable.roundprofilepic));
         }
 
@@ -92,6 +139,7 @@ public class profile_page extends Fragment {
         Button addFriendButton=myview.findViewById(R.id.add_friend_button);
         Button messageButton=myview.findViewById(R.id.message_profile);
         Button recommendButton=myview.findViewById(R.id.give_rec_profile);
+
         ViewGroup layout = (ViewGroup) messageButton.getParent();
 
         if(profileType=="PERSONAL") {
@@ -107,18 +155,17 @@ public class profile_page extends Fragment {
         else{
             layout.removeView(addFriendButton);
         }
-        if(arguments.containsKey("updatedName")) {
-            TextView name = myview.findViewById(R.id.profile_pic_name);
-            TextView interests = myview.findViewById(R.id.profile_interests);
-
-            String newname = arguments.getString("updatedName");
-            if (newname != "")
-                name.setText(newname);
-
-            String newInterests = arguments.getString("updatedInterests");
-            if (newInterests != "")
-                interests.setText(newInterests);
-        }
+//        if(arguments.containsKey("updatedName")) {
+//
+//
+//            String newname = arguments.getString("updatedName");
+//            if (newname != "")
+//                name.setText(newname);
+//
+//            String newInterests = arguments.getString("updatedInterests");
+//            if (newInterests != "")
+//                interests.setText(newInterests);
+//        }
 
         scrollFunction(R.id.sample_favourite_movie,moviesArray,profileType);
         scrollFunction(R.id.sample_watch_list,movies2Array,profileType);
